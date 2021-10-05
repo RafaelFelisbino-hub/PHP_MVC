b712fcdb7a5e3cfd6742b9fba953982aa9f3de6c
Rafael Felisbino Rocha da Silva <72631228+RafaelFelisbino-hub@users.noreply.github.com>
2021-10-05 12:15:32 -0300

    sql atualizado com FK

--- sql/sa_03_crud_database.sql
+++ sql/sa_03_crud_database.sql
@@ -8,13 +8,15 @@ create table organizacao(
 );
 
 create table procedimento(
-id_consulta int not null auto_increment primary key,
-codigo int,
-nome varchar(50),
-valor double,
-genero varchar(10),
-excecao varchar(200),
-data_procedimento date
+	id_consulta int not null auto_increment primary key,
+	codigo int,
+	nome varchar(50),
+	valor double,
+	genero varchar(10),
+	excecao varchar(200),
+	data_procedimento date,
+	paciente_id_paciente int(5),
+	FOREIGN KEY (paciente_id_paciente) REFERENCES tbl_paciente(id_paciente)
 );
 
  CREATE TABLE tbl_especialidade(
@@ -35,28 +37,28 @@ CREATE TABLE tbl_paciente(
   );
 
 create table funcionamento (
-id_funcionamento int(5) primary key auto_increment not null,
-abertura_clinica varchar (6),
-fechamento_clinica VARCHAR(6),
-media_consultas VARCHAR(6),
-dias_funcionamento VARCHAR(20)
+	id_funcionamento int(5) primary key auto_increment not null,
+	abertura_clinica varchar (6),
+	fechamento_clinica VARCHAR(6),
+	media_consultas VARCHAR(6),
+	dias_funcionamento VARCHAR(20)
 );
 
 create table convenios(
-id_convenio int(5) auto_increment primary key,
-nome_fantasia varchar(40),
-nome_empresa varchar(40),
-cnpj char(14),
-email varchar(40),
-nome_cont varchar(40),
-telefone VARCHAR(15),
-celular VARCHAR(16),
-endereco varchar(40),
-numero INT(10),
-complemento varchar(40),
-cidade varchar(40),
-estado varchar(40),
-cep varchar(9)
+	id_convenio int(5) auto_increment primary key,
+	nome_fantasia varchar(40),
+	nome_empresa varchar(40),
+	cnpj char(14),
+	email varchar(40),
+	nome_cont varchar(40),
+	telefone VARCHAR(15),
+	celular VARCHAR(16),
+	endereco varchar(40),
+	numero INT(10),
+	complemento varchar(40),
+	cidade varchar(40),
+	estado varchar(40),
+	cep varchar(9)
 );
 
 CREATE TABLE medicos (
@@ -68,5 +70,9 @@ CREATE TABLE medicos (
     bairro_medico VARCHAR(45) NOT NULL,
     cep_medico CHAR(9) NOT NULL,
     email_medico VARCHAR(45) NOT NULL,
-    telefone_fixo_medico VARCHAR(15) NOT NULL
+    telefone_fixo_medico VARCHAR(15) NOT NULL,
+    paciente_id_paciente INT(5),
+    FOREIGN KEY (paciente_id_paciente) REFERENCES tbl_paciente(id_paciente)
 );
+
+drop table procedimento;
